f02b0ff88d74f57f7d4f3cd9aadfee4d57a56504
Siphelele <sipheleledanisa90@gmail.com>
2022-07-23 13:19:45 -0400

    fixing the init file

--- code/maci/learners/__init__.py
+++ code/maci/learners/__init__.py
@@ -1,8 +1,8 @@
 # from .sac import SAC
 # from .diayn import DIAYN
-from .masql_ksd import MASQL_svgd
-from .masql_lsd import MASQL_ksd
-from .masql_svgd import MASQL_lsd
+from .masql_ksd import MASQL_ksd
+from .masql_lsd import MASQL_lsd
+from .masql_svgd import MASQL_svgd
 from .maddpg import MADDPG
 from .mavb_ac import MAVBAC
 from .mi_ac import MI
